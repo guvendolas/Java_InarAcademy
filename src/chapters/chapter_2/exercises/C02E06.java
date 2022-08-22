cf16a9731c7b53eaf379d4e5f6abdce4177bc6bc
inar@hotmail.com <inar@hotmail.com>
2022-08-22 19:42:04 +0300

    added chapter 2 exercises 1-6

---
+++ src/chapters/chapter_2/exercises/C02E06.java
@@ -0,0 +1,29 @@
+package chapters.chapter_2.exercises;
+
+import java.util.Scanner;
+
+public class C02E06 {
+	public static void main(String[] args) {
+		int number;
+		int sum = 0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number between 0 and 1000: ");
+		number = input.nextInt();
+		
+		sum = sum + (number % 10);
+		number = number / 10;
+		
+		sum = sum + (number % 10);
+		number = number /10;
+		
+		sum = sum + (number % 10);
+		
+		System.out.println("The sum of all digits is " + sum);
+		
+		
+		
+	}
+	
+
+}
