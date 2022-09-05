2939b5ffd892ecec3c1845aa7c7c54548ca971f2
guvendolas <guvenndolas@gmail.com>
2022-09-05 20:27:23 +0300

    modifeded

--- src/chapters/chapter_4/exercises/C04E22.java
+++ src/chapters/chapter_4/exercises/C04E22.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class C04E22 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 
 		System.out.print("Enter string s1:");
-		String s1 = input.nextLine();
+		String s1 = in.nextLine();
 		System.out.print("Enter string s2:");
-		String s2 = input.nextLine();
+		String s2 = in.nextLine();
 
 		if (s1.contains(s2)) {
 			System.out.println(s2 + " is a substring of " + s1);
 		} else {
 			System.out.println(s2 + " is not a substring of " + s1);
 		}
-		input.close();
+		in.close();
 	}
 
 }
